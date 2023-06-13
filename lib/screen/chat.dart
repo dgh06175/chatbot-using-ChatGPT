6cb35c7f564ba808f91c34595983e3497a9698be
dgh06175 <dgh06175@gmail.com>
2023-06-13 20:24:01 +0900

    add chat message UI

---
+++ lib/screen/chat.dart
@@ -0,0 +1,72 @@
+import '../chat/chat_model.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessageWidget extends StatelessWidget {
+  const ChatMessageWidget(
+      {super.key, required this.text, required this.chatMessageType});
+
+  final String text;
+  final ChatMessageType chatMessageType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(16),
+      color: chatMessageType == ChatMessageType.bot
+          ? ColorSets.botBackgroundColor
+          : ColorSets.backgroundColor,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          chatMessageType == ChatMessageType.bot
+              ? Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    backgroundColor: const Color.fromRGBO(16, 163, 127, 1),
+                    child: Image.asset(
+                      'assets/chatgpt.png',
+                      color: Colors.white,
+                      scale: 1.5,
+                    ),
+                  ),
+                )
+              : Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: const CircleAvatar(
+                    backgroundColor: const Color.fromRGBO(70, 70, 70, 1),
+                    child: Icon(
+                      Icons.person,
+                    ),
+                  ),
+                ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  ),
+                  child: Text(
+                    text,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorSets {
+  static const backgroundColor = Color(0xff656565);
+  static const botBackgroundColor = Color(0xff404040);
+}
