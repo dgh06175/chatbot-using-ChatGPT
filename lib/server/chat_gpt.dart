0878b320ee608829b787302a6bf9236fb42bc4e3
dgh06175 <dgh06175@gmail.com>
2023-06-14 02:40:49 +0900

    gpt 모델 호출할때 입력받은 값 사용

--- lib/server/chat_gpt.dart
+++ lib/server/chat_gpt.dart
@@ -2,10 +2,11 @@ import 'package:http/http.dart' as http;
 import 'package:chatbot/util/keys.dart';
 import 'dart:convert';
 
-const systemContent =
-    "You are a chatbot that recommends replying to messages. Your role is the user\'s friend. If the user asks for an explanation you should explain it for your past answer. If not, just tell him the recommended reply without explanation. use Korean language";
+Future<String> sendMessageToChatGPT(String message, String relationText) async {
+  const String systemContent =
+      "You are a robot that recommends what User should reply to text messages from someone. use korean.";
+  String assistantContent = "유저는 $relationText에게 문자를 받았어.";
 
-Future<String> sendMessageToChatGPT(String message) async {
   var url = Uri.https("api.openai.com", "/v1/chat/completions");
   final response = await http.post(
     url,
@@ -18,11 +19,10 @@ Future<String> sendMessageToChatGPT(String message) async {
       "messages": [
         {
           "role": "system",
-          "content":
-              "You are a robot that recommends what User should reply to text messages from someone. use korean."
+          "content": systemContent,
         },
         {"role": "user", "content": message},
-        {"role": "assistant", "content": "유저는 여자친구 에게 메세지를 받았어."}
+        {"role": "assistant", "content": assistantContent}
       ],
       // "temperature": "2",
     }),
